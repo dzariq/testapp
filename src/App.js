7f88fb8f5ca982a6badf6ad17051755a7b134384
dzariq mirza <dzariq85@gmail.com>
2023-10-30 22:13:12 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,75 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Component } from 'react';
+import { useEffect }
+from 'react';
+import { useState }
+from 'react';
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Component }
+from 'react';
 import axios from 'axios';
 
-class App extends Component {
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import CareersLayout from './layouts/CareersLayout'
+import Home from './pages/Home'
+import Payment from './pages/Payment'
+import Faq from './pages/help/Faq'
+import NotFound from './pages/NotFound'
+import Contact from './pages/help/Contact'
+import Careers from './pages/careers/Careers'
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: []
-        }
-        this.api_url = 'https://nlp.fantasyligasuper.com';
-    }
+        const router = createBrowserRouter(
+                createRoutesFromElements(
+                        <Route path="/" element={ < RootLayout / > }  >
+                            <Route index element={ < Home / > } />
+                            <Route path="payment/:orderId" element={ < Payment / > } />
+                            <Route path="help" element={ < HelpLayout / > }>
+                                <Route path="faq" element={ < Faq / > } />
+                                <Route path="contact" element={ < Contact / > } />
+                            </Route>
+                            <Route path="careers" element={ < CareersLayout / > }>
+                                <Route index element={ < Careers / > } />
+                            </Route>
+                            
+                            <Route path="*" element={<NotFound />} />
+                        </Route>
+                                                    )
+                                            );
 
-    //init
-    componentDidMount() {
-        this.refreshUsers()
-    }
+                            class App extends Component {
 
-    async refreshUsers() {
-        const response = await axios.get(this.api_url + '/users')
-        const data = await response.data
-        this.setState({
-            users: data,
-        })
-    }
+                                constructor(props) {
+                                    super(props);
+                                    this.state = {
+                                        users: []
+                                    }
+                                    this.api_url = 'https://nlp.fantasyligasuper.com';
+                                }
 
-    async addClick() {
-        var userName = document.getElementById('userName').value;
-        const data = new FormData();
+//init
+                                componentDidMount() {
+                                  //  this.refreshUsers()
+                                }
 
-        data.append("name", userName);
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        const res = await axios.post(this.api_url + '/add', data, config)
-        const resData = await res.resData
-        alert('SUCCESS ADD')
-        this.refreshUsers();
+                                async refreshUsers() {
+                                    const response = await axios.get(this.api_url + '/users')
+                                    const data = await response.data
+                                    this.setState({
+                                        users: data,
+                                    })
+                                }
 
-    }
+                                async addClick() {
+                                    var userName = document.getElementById('userName').value;
+                                    const data = new FormData();
+                                    data.append("name", userName);
+                                    const config = {
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                    };
+                                    const res = await axios.post(this.api_url + '/add', data, config)
+                                    const resData = await res.resData
+                                    alert('SUCCESS ADD')
+                                    this.refreshUsers();
+                                }
 
-    async deleteClick(id) {
-        const res = await axios.delete(this.api_url + '/delete?user_id=' + id)
-        const resData = await res.resData
-        alert('SUCCESS DELETE')
-        this.refreshUsers();
+                                async deleteClick(id) {
+                                    const res = await axios.delete(this.api_url + '/delete?user_id=' + id)
+                                    const resData = await res.resData
+                                    alert('SUCCESS DELETE')
+                                    this.refreshUsers();
+                                }
 
-    }
+                                render() {
+                                    const{users} = this.state;
+                                    return (
+                                            <RouterProvider router={router}  />
+                                            );
+                                }
 
-    render() {
-        const{users} = this.state;
-        return (
-                <div className="App">
-                    <h2>BADA APP</h2>
-                    <input id="userName" />
-                    <button onClick={() => this.addClick()} >Add User</button>
-                    {users.map(user =>
-                                <p>
-                                    <b>{user.name}</b>
-                                    <button onClick={() => this.deleteClick(user.user_id)} >Delete User</button>
-                                </p>
-                        )}
-                </div>
-                );
-    }
+                            }
 
-}
-
-export default App;
+                            export default App;
