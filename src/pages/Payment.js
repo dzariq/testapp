0edb845a53f4c431289edffb9aa36ef441df6c9c
dzariq mirza <dzariq85@gmail.com>
2023-10-31 16:27:54 +0800

    updates

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,24 +1,42 @@
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function Payment() {
-   const searchParams = new URLSearchParams(window.location.search);
+    const searchParams = new URLSearchParams(window.location.search);
     const orderId = searchParams.get('order_id');
 
-        const api_url = 'https://nlp.fantasyligasuper.com';
-    const [users,setData] = useState([])
+    const api_url = 'https://nlp.fantasyligasuper.com';
+    const [payment, setData] = useState([]);
+    const [actionUrl, setUrl] = useState([]);
 
-    useEffect(() =>  {
-      axios.get(api_url + '/payment/'+orderId)
-             .then(res => setData(res.data))
-             .catch(err => console.log(err))
- },[]);
+    
+    useEffect(() => {
+        axios.get(api_url + '/payment/' + orderId)
+                .then(response => {
+                    console.log(response.data[0].formcode)
+                    setData(response.data[0])
+                    setUrl(`https://app.senangpay.my/payment/${response.data[0].formcode}`)
+                })
+                .catch(err => console.log(err))
 
-return (
-        <div className="about">
- <h2>Parameter Value: {orderId}</h2>
-</div>
-)
+        const form = document.getElementById('order');
+       form.submit();
+    }, []);
+
+    return (
+            <form id="order" method="post" action={actionUrl}>
+                <input type="hidden" name="detail" value={payment.detail} />
+                <input type="hidden" name="amount" value={payment.amount}/>
+                <input type="hidden" name="order_id" value={payment.order_id}/>
+                <input type="hidden" name="name" value={payment.name}/>
+                <input type="hidden" name="email" value={payment.email}/>
+                <input type="hidden" name="phone" value={payment.phone}/>
+                <input type="hidden" name="hash" value={payment.hash}/>
+            </form>
+            )
+
+
+
 }
