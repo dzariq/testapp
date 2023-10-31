21bbfac7e9869d6874dda9b6677f27ccfebf9566
dzariq mirza <dzariq85@gmail.com>
2023-10-31 17:13:58 +0800

    updates

---
+++ public/botpress.js
@@ -0,0 +1,13 @@
+window.botpressWebChat.init({
+    "composerPlaceholder": "Chat with bot",
+    "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+    "botId": "189463f9-0dba-4dbd-88e7-80ba257842db",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v0",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "189463f9-0dba-4dbd-88e7-80ba257842db",
+    "lazySocket": true,
+    "frontendVersion": "v0",
+    "showPoweredBy": true,
+    "theme": "prism",
+    "themeColor": "#2563eb"
+});
