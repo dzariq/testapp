65060f009dfc55a5ebb7c437fdb83d791481a054
dzariq mirza <dzariq85@gmail.com>
2023-10-31 17:22:49 +0800

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,15 +14,6 @@ export default function Home() {
 
     return (
             <div className="users">
-                <h2>BADA APP</h2>
-                <input id="userName" />
-                <button onClick={() => this.addClick()} >Add User</button>
-                { users.map(user =>
-                        <p>
-                            <b>{user.name}</b>
-                            <button onClick={() => this.deleteClick(user.user_id)} >Delete User</button>
-                        </p>
-                )}
             </div>
             );
 }
