2b305d70167f643249bd465b2d77d181407fba1e
Dzariq <dzariq85@gmail.com>
2024-02-07 15:55:39 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect,useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
     const api_url = 'https://nlp.fantasyligasuper.com';
